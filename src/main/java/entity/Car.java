7cf76218dbdce7b181e6f0ab5788914c70019aed
stevedell98 <stevedel98@gmail.com>
2024-09-13 00:03:21 +0300

    car spring data jpa with one method(find all)

---
+++ src/main/java/entity/Car.java
@@ -0,0 +1,64 @@
+package entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="car")
+public class Car {
+
+    @Id
+    @Column(name = "brand")
+    private String brandName;
+
+    @Column(name = "model")
+    private String modelName;
+
+    @Column(name = "year")
+    private String year;
+
+    public Car(){
+
+    }
+
+    public Car(String modelName, String year) {
+        this.modelName = modelName;
+        this.year = year;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                ", brand '" + brandName + '\'' +
+                ", model'" + modelName + '\'' +
+                ", year'" + year + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/service/Car_service.java
@@ -0,0 +1,18 @@
+package service;
+
+import entity.Car;
+
+import java.util.List;
+
+
+public interface Car_service {
+
+    public List<Car> findAll();
+
+    //public Car getCarbyId(String Id);
+
+    //public Car updateCars(Car carBrand);
+
+    //public void deleteCar(String Id);
+}
+
