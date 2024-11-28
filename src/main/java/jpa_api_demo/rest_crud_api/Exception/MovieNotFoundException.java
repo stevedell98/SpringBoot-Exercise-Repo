03af7595c45cd5bf8009695a9c77403eba900fca
stevedell98 <stevedel98@gmail.com>
2024-11-29 01:44:36 +0200

    Deleted extra DAOs

--- src/main/java/jpa_api_demo/rest_crud_api/Exception/MovieNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package jpa_api_demo.rest_crud_api.Exception;
-
-
-public class MovieNotFoundException extends RuntimeException{
-
-    public MovieNotFoundException(String message){
-        super(message);
-    }
-}
--- src/main/java/jpa_api_demo/rest_crud_api/MovieNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package jpa_api_demo.rest_crud_api;
-
-public class MovieNotFoundException extends RuntimeException{
-
-    public MovieNotFoundException(String message) {
-        super(message);
-    }
-}
